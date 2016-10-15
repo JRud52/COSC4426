4ce76a7431a032dd2360ceddb98c8801175af278
Justin Rhude <JRud52@localhost.localdomain>
2016-10-15 19:19:52 -0400

    completed assignment 4

---
+++ Assignment_4/main.php
@@ -0,0 +1,158 @@
+<?php
+    session_start();
+    if (!isset($_SESSION["user"])){
+        echo "<script>window.location = 'https://justinrhude.com'</script>";
+    }
+?>
+
+    <!doctype html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="expires" content="0">
+        <title>Portfolio of Justin Rhude</title>
+        <meta name="description" content="The portfolio of Justin Rhude.">
+        <link rel="stylesheet" href="./mdl/material.min.css">
+        <script src="./mdl/material.min.js"></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+
+    <body>
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
+            mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                    <div class="mdl-layout-spacer"></div>
+                    <label>Welcome <?php echo $_SESSION['user'] ?>!</label>
+                    <a class='logout-btn mdl-button mdl-js-button mdl-button--primary ' href='?logoff'>Logout</a>
+                </div>
+            </header>
+            <div class="mdl-layout__drawer"> <span class="mdl-layout-title">Justin Rhude</span>
+                <nav class="mdl-navigation">
+                    <a class="mdl-navigation__link" href="">Link1</a>
+                    <a class="mdl-navigation__link" href="">Link2</a>
+                    <a class="mdl-navigation__link" href="">Link3</a>
+                    <a class="mdl-navigation__link" href="">Link4</a>
+                </nav>
+            </div>
+            <main class="mdl-layout__content">
+                <div class="page-content">
+                    <div class='mdl-card mdl-shadow--4dp main-card'>
+                        <?php
+                        echo "<h3>You are logged in as: ".$_SESSION['user']."</h3>";
+                        ?>
+                    </div>
+                    <?php if ($_SESSION['access_level'] == 0){
+                    echo "<div class='mdl-card mdl-shadow--4dp main-card'>";
+                    echo "<h4>Change a user's permissions:</h4>";
+
+                            $servername = "db.justinrhude.com";
+                            $db_username = "jr_db_access";
+                            $db_password = "helloworld3";
+                            $db_name = "jr_logins";
+
+                            try {
+                               $conn = new PDO("mysql:host=$servername;dbname=$db_name", $db_username, $db_password);
+                               $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                               if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['username'])) {
+                                   $sql = $conn->prepare("UPDATE users SET privilages=".$_POST['acl_'.$_POST["username"].'']." WHERE username='".$_POST["username"]."'");
+                                   $sql->execute();
+                               }
+
+                                $sql = $conn->prepare("SELECT username, privilages FROM users");
+                                $sql->execute();
+                                $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+                                foreach ($result as $row => $list_item){
+                                    echo '<form action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" method="POST">';
+                                    echo '<label>Username: '.$list_item["username"].'</label>';
+                                    if ($list_item['privilages'] == 0){
+                                        echo '<div class="float-right">
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="0" checked disabled/>Admin
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="1" disabled/>Moderator
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="2" disabled/>User';
+                                    }
+                                    elseif ($list_item['privilages'] == 1){
+                                        echo '<div class="float-right">
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="0" disabled/>Admin
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="1" checked/>Moderator
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="2" />User';
+                                    }
+                                    else {
+                                        echo '<div class="float-right">
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="0" disabled/>Admin
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="1" />Moderator
+                                        <input type="radio" name="acl_'.$list_item['username'].'" value="2" checked/>User';
+                                    }
+                                    echo '<button type="submit" class="mdl-button mdl-js-button mdl-button--colored logout-btn">Update</button></div>';
+                                    echo '<input type="text" name="username" value="'.$list_item['username'].'" hidden/></form>';
+                                }
+                           }
+                           catch(PDOException $e) {
+                               echo "Something went wrong...";
+                           }
+                            $conn = null;
+
+                    echo '</div>';
+                    }?>
+
+                    <div class='mdl-card mdl-shadow--4dp main-card'>
+                        <h4>Add a comment:</h4>
+                        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                            <input type="text" name='comment_input' placeholder="Comment" />
+                            <button class='mdl-button mdl-js-button mdl-button--colored' type="submit">Submit</button>
+                        </form>
+                        <?php
+                        $servername = "db.justinrhude.com";
+                        $db_username = "jr_db_access";
+                        $db_password = "helloworld3";
+                        $db_name = "jr_logins";
+
+                        try {
+                           $conn = new PDO("mysql:host=$servername;dbname=$db_name", $db_username, $db_password);
+                           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                           if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['comment_input'])){
+                               $sql = $conn->prepare("INSERT INTO Comments (username, post_time, comment)
+                                                        VALUES ('".$_SESSION['user']."', '".date('Y/m/d H:i:s')."', '".$_POST['comment_input']."')");
+                               $sql->execute();
+                           }
+                           if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['comment_id'])){
+                               $sql = $conn->prepare("DELETE FROM Comments WHERE comment_id='".$_POST['comment_id']."'");
+                               $sql->execute();
+                           }
+
+                            $sql = $conn->prepare("SELECT comment, username, post_time, comment_id FROM Comments ORDER BY post_time DESC");
+                            $sql->execute();
+                            $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+                            foreach ($result as $row => $list_item){
+                                echo $list_item["username"].": ".$list_item['post_time']."-> ".$list_item['comment']."<br/>";
+                                if ($_SESSION['access_level'] <= 1){
+                                    echo "  <form method='POST' action='".htmlspecialchars($_SERVER['PHP_SELF'])."'>
+                                                <input type='text' name='comment_id' value='".$list_item['comment_id']."' hidden/>
+                                                <button class='mdl-button mdl-js-button mdl-button--colored' type='submit'>Remove</button>
+                                            </form>";
+                                }
+                            }
+                        }
+                        catch(PDOException $e) {
+                            echo "Something went wrong...".$e;
+                        }
+                         $conn = null;
+                         ?>
+                    </div>
+
+
+                    <?php
+                    if (isset($_GET['logoff'])){
+                        session_unset();
+                        session_destroy();
+                        echo "<script>window.location = ' https://justinrhude.com '</script>";
+                    }
+                    ?>
+                </div>
+            </main>
+        </div>
+    </body>
+
+    </html>
---
+++ Assignment_4/register.php
@@ -0,0 +1,152 @@
+<?php
+    session_start();
+    if (isset($_SESSION["user"])){
+        echo "<script>window.location = 'https://justinrhude.com/main.php'</script>";
+    }
+?>
+
+    <!doctype html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="expires" content="0">
+        <title>Portfolio of Justin Rhude</title>
+        <meta name="description" content="The portfolio of Justin Rhude.">
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link rel="stylesheet" href="./mdl/material.min.css">
+        <script src="./mdl/material.min.js"></script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    </head>
+
+
+    <body>
+        <div class="mdl-layout mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                    <!-- Title -->
+                    <span class="mdl-layout-title">Portfolio of Justin Rhude</span>
+                </div>
+            </header>
+            <div class="mdl-layout__drawer"> <span class="mdl-layout-title">Justin Rhude</span>
+                <nav class="mdl-navigation">
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                    <a class="mdl-navigation__link" href="">Link</a>
+                </nav>
+            </div>
+            <main class="mdl-layout__content">
+                <div class="page-content">
+                    <div class='mdl-card mdl-shadow--4dp login-form'>
+                        <div class='mdl-card__actions'> </div>
+                        <form action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>' method='POST'>
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="mdl-textfield__input" type="email" name="email-input">
+                                <label class="mdl-textfield__label" for="email-input">Email:</label>
+                                <span class="mdl-textfield__error">This is required.</span>
+                            </div>
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="pass-input mdl-textfield__input" type="text" name="user-input">
+                                <label class="mdl-textfield__label" for="user-input">Username:</label>
+                                <span class="mdl-textfield__error">This is required.</span>
+                            </div>
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="pass-input mdl-textfield__input" type="password" name="pass-input">
+                                <label class="mdl-textfield__label" for="pass-input">Password:</label>
+                                <span class="mdl-textfield__error">This is required.</span>
+                            </div>
+
+                            <select name="security-question" required>
+                                <option value='1' selected>Mother's maiden name?</option>
+                                <option value='2'>Father's middle name?</option>
+                                <option value='3'>Favourite colour?</option>
+                                <option value='4'>Favourite movie?</option>
+                            </select>
+
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                <input class="pass-input mdl-textfield__input" type="text" name="answer-input">
+                                <label class="mdl-textfield__label" for="answer-input">Answer:</label>
+                                <span class="mdl-textfield__error">This is required.</span>
+                            </div>
+                            <button class='login-btn mdl-button mdl-js-button mdl-button--raised mdl-button--colored' type="submit">Register</button>
+                        </form>
+                    </div>
+                    <?php
+                $sq = $email = $username = $password = $answer = "" ;
+                if ($_SERVER[ "REQUEST_METHOD"]=="POST" ) {
+                    $continue=true;
+                    if (empty($_POST[ "email-input"])) {
+                        $continue=false;
+                    }
+                    else {
+                        $email=fix_input($_POST[ "email-input"]);
+                    }
+                    if (empty($_POST[ "user-input"])) {
+                        $continue=false;
+                    }
+                    else {
+                        $username=fix_input($_POST[ "user-input"]);
+                    }
+                    if (empty($_POST[ "pass-input"])) {
+                        $continue=false;
+                    }
+                    else {
+                        $password=fix_input($_POST[ "pass-input"]);
+                    }
+                    if (empty($_POST[ "security-question"])) {
+                        $continue=false;
+                    }
+                    else {
+                        $sq=fix_input($_POST[ "security-question"]);
+                    }
+                    if (empty($_POST[ "answer-input"])) {
+                        $continue=false;
+                    }
+                    else {
+                        $answer=fix_input($_POST[ "answer-input"]);
+                    }
+
+                    if ($continue==true) {
+                        connect_db($email, $username, $password, $sq, $answer);
+                    }
+                    else {
+                        echo "Please fill in all the fields.";
+                    }
+
+                }
+                function fix_input($data) {
+                    $data=trim($data);
+                    $data=stripslashes($data);
+                    $data=htmlspecialchars($data);
+                    return $data;
+                }
+                function connect_db($email, $user, $pass, $sq, $ans) {
+                    $servername="db.justinrhude.com";
+                    $db_username="jr_db_access";
+                    $db_password="helloworld3";
+                    $db_name="jr_logins";
+                    $pass_hash= password_hash($pass, PASSWORD_DEFAULT);
+                    $ans_hash= password_hash($ans, PASSWORD_DEFAULT);
+
+                    try {
+                        $conn=new PDO( "mysql:host=$servername;dbname=$db_name", $db_username, $db_password);
+                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        $sql = "INSERT INTO users (email, username, password, question, answer, privilages) VALUES ('$email', '$user', '$pass_hash', '$sq', '$ans_hash', '2')";
+                        // use exec() because no results are returned
+                        $conn->exec($sql);
+                        $_SESSION["user"] = $user;
+                        echo "<script>window.location = 'https://justinrhude.com/main.php'</script>";
+                    }
+                    catch(PDOException $e) {
+                        echo "That username is already taken...";
+                    }
+                    $conn = null;
+                }
+                ?>
+                </div>
+            </main>
+        </div>
+    </body>
+
+    </html>
